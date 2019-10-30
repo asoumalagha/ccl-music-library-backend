d78f1ee74e6d253baaf83a69f4ea3af3de812683
asoumalagha <asoumalagha@gmail.com>
2019-10-30 12:09:28 +0100

    Default songs are added from memory

--- src/main/java/com/codecool/cclmusiclibrarybackend/config/CreatorConfig.java
+++ src/main/java/com/codecool/cclmusiclibrarybackend/config/CreatorConfig.java
@@ -19,7 +19,14 @@ public class CreatorConfig {
     @Bean
     public CommandLineRunner init() {
         return args -> {
-            songStorage.addSong(songCreator.createSong("example", "example", "example", 1));
+            songStorage.addSong(songCreator.createSong("Cherri cherri lady", "Let's talk about love", "Modern talking", 3.46));
+            songStorage.addSong(songCreator.createSong("Relax", "Frankie goes to Hollywood", "example", 3.56));
+            songStorage.addSong(songCreator.createSong("Toxic", "In the zone", "Britney Spears", 3.18));
+            songStorage.addSong(songCreator.createSong("...Baby one more time", "Britney Spears", "Baby one more time", 3.31));
+            songStorage.addSong(songCreator.createSong("Beautiful trauma", "What about us", "P!nk", 4.57));
+            songStorage.addSong(songCreator.createSong("Just give me reasons", "Nate rules", "P!nk", 3.21));
+            songStorage.addSong(songCreator.createSong("Manicure", "Art pop", "Lady Gaga", 3.19));
+            songStorage.addSong(songCreator.createSong("Jump around", "House of Pain", "House of pain", 3.34));
         };
 
     }
