bd1de7e421efcbd93669c0ba75b2e3312941b9d8
Szabó Gergő <Szabogergo1992@gmail.com>
2019-11-12 11:11:41 +0100

    lombok dependency added to pom.xml, Song.java updated with lombok usage

--- src/main/java/com/codecool/cclmusiclibrarybackend/model/Song.java
+++ src/main/java/com/codecool/cclmusiclibrarybackend/model/Song.java
@@ -1,5 +1,12 @@
 package com.codecool.cclmusiclibrarybackend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Song {
 
     private String title;
@@ -7,55 +14,4 @@ public class Song {
     private String performer;
     private double length;
 
-    public Song(String title, String album, String performer, double length) {
-        this.title = title;
-        this.album = album;
-        this.performer = performer;
-        this.length = length;
-    }
-
-    public Song() {
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAlbum(String album) {
-        this.album = album;
-    }
-
-    public void setPerformer(String performer) {
-        this.performer = performer;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAlbum() {
-        return album;
-    }
-
-    public String getPerformer() {
-        return performer;
-    }
-
-    public double getLength() {
-        return length;
-    }
-
-    @Override
-    public String toString() {
-        return "Song{" +
-                "title='" + title + '\'' +
-                ", album='" + album + '\'' +
-                ", performer='" + performer + '\'' +
-                ", length=" + length +
-                '}';
-    }
 }
