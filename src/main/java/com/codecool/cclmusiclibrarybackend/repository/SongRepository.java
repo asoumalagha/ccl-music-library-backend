77099efb1486aad682ce282f8dc3ca6d94b537db
asoumalagha <asoumalagha@gmail.com>
2019-11-14 12:58:02 +0100

    delete song by id finally works

--- src/main/java/com/codecool/cclmusiclibrarybackend/repository/SongRepository.java
+++ src/main/java/com/codecool/cclmusiclibrarybackend/repository/SongRepository.java
@@ -3,6 +3,10 @@ package com.codecool.cclmusiclibrarybackend.repository;
 import com.codecool.cclmusiclibrarybackend.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
 public interface SongRepository extends JpaRepository<Song, Long> {
+
+    @Transactional
     void deleteSongById(Long id);
 }
