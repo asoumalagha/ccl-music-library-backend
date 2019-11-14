0c8c3421be728f742e0cea4f456082b10b7d5178
asoumalagha <asoumalagha@gmail.com>
2019-11-14 20:32:43 +0100

    7/5 tests are works

--- src/test/java/com/codecool/cclmusiclibrarybackend/service/SongHandlerTest.java
+++ src/test/java/com/codecool/cclmusiclibrarybackend/service/SongHandlerTest.java
@@ -22,47 +22,57 @@ public class SongHandlerTest {
     @Autowired
     private SongHandler songHandler;
 
+    @Autowired
+    private SongRepository songRepository;
+
+    private Song example = Song.builder()
+            .id((long) 1)
+            .title("example")
+            .length(6)
+            .album("example")
+            .performer("example")
+            .build();
+
 
     @Test
     public void testGetAllSongs(){
         List<Song> songs = songHandler.getSongs();
-        assertTrue(songs.size()>0);
+        assertEquals(9, songs.size());
     }
 
     @Test
     public void testGetSongByParameters() throws Exception {
-        Song song1 = Song.builder()
-                .id((long) 1)
-                .title("example")
-                .length(6)
-                .album("example")
-                .performer("example")
-                .build();
         Song foundedSong = songHandler.getSong("example", "example","example", 6);
-        assertEquals(song1, foundedSong);
-
+        assertEquals(example, foundedSong);
     }
 
     @Test
     public void testGetSongByObject() throws Exception {
-        Song song1 = Song.builder()
-                .id((long) 1)
+        Song foundedSong = songHandler.getSong(example);
+        assertEquals(example, foundedSong);
+    }
+
+    @Test
+    public void testAddSong(){
+        Song example = Song.builder()
+                .id((long) 20)
                 .title("example")
                 .length(6)
                 .album("example")
                 .performer("example")
                 .build();
-        Song foundedSong = songHandler.getSong(song1);
-        assertEquals(song1, foundedSong);
+        songHandler.addSong(example);
+        int size = songHandler.getSongs().size();
+        assertEquals(10, size);
+        songRepository.delete(example);
     }
 
     @Test
     public void testDeleteSongById(){
+        songRepository.save(example);
+        songHandler.deleteSong((long) 20);
         List<Song> songs = songHandler.getSongs();
         assertEquals(9, songs.size());
-        songHandler.deleteSong((long) 2);
-        songs = songHandler.getSongs();
-        assertEquals(8, songs.size());
     }
 
 
--- src/test/java/com/codecool/cclmusiclibrarybackend/service/UserHandlerTest.java
+++ src/test/java/com/codecool/cclmusiclibrarybackend/service/UserHandlerTest.java
@@ -1,12 +1,48 @@
 package com.codecool.cclmusiclibrarybackend.service;
 
+import com.codecool.cclmusiclibrarybackend.model.User;
+import com.codecool.cclmusiclibrarybackend.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
 public class UserHandlerTest {
+
+    @Autowired
+    private UserHandler userHandler;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private User example = User.builder()
+            .username("Példa")
+            .password("geza")
+            .firstName("Példa")
+            .lastName("Géza")
+            .email("pelda@geza.hu")
+            .build();
+
+    @Test
+    public void testAddUserByObject(){
+        userHandler.addUser(example);
+        List<User> users = userRepository.findAll();
+        assertEquals(2, users.size());
+        userRepository.delete(example);
+    }
+
+    @Test
+    public void testAddUserByHand(){
+        userHandler.addUser("Mégegy", "megegy", "Mégegy", "és", "meg@egy.hu");
+        List<User> users = userRepository.findAll();
+        assertEquals(2, users.size());
+    }
 
 
 }
