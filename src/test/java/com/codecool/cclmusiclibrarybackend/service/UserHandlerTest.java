5140647fc881cce748d2f5166197e8ea5d15f83d
asoumalagha <asoumalagha@gmail.com>
2019-11-14 13:19:45 +0100

    Test branch created

---
+++ src/test/java/com/codecool/cclmusiclibrarybackend/service/UserHandlerTest.java
@@ -0,0 +1,12 @@
+package com.codecool.cclmusiclibrarybackend.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserHandlerTest {
+
+
+}
