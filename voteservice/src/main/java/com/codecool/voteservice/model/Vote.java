2a83d7b0b3d502a026e7535ee48deafcfbcfce99
Szabó Gergő <Szabogergo1992@gmail.com>
2019-12-12 15:53:58 +0100

    default value removed from Vote entity's rating column

--- voteservice/src/main/java/com/codecool/voteservice/model/Vote.java
+++ voteservice/src/main/java/com/codecool/voteservice/model/Vote.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -21,7 +20,6 @@ public class Vote {
     @GeneratedValue
     private Long id;
 
-    @Column(columnDefinition="int default '0'")
     private int rating;
 
     private Long songId;
